2e4e415542e31a6dd7e56233ffcc455e378bd357
Ean <79337127+CarlosErz@users.noreply.github.com>
2022-02-02 12:24:47 -0600

    Add files via upload

---
+++ js/pag.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu");
+const indicador = document.getElementById("indicador");
+const seccion = document.querySelector(".seccion");
+
+let tamIndicador = menu.querySelector("a").offsetWidth;
+
+
+
+
+
+
+
